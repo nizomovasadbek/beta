7c977984517445b2c3f392cc26c61f85d3360aec
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-07-27 10:08:53 +0500

    Deleteing by ID

--- src/main/java/uz/beta/teacher/Teacher.java
+++ src/main/java/uz/beta/teacher/Teacher.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import uz.beta.util.Subject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,6 +18,18 @@ public class Teacher {
     private Integer teacherId;
     private String firstName;
     private String lastName;
+    private List<Subject> subjects = new ArrayList<>();
 
+    public void addSubjects(Subject sub){
+        subjects.add(sub);
+    }
+
+    public void removeSubject(Subject sub){
+        subjects.removeIf(pr -> sub.getSubjectId().equals(pr.getSubjectId()));
+    }
+
+    public void removeSubjectById(Integer id){
+        subjects.removeIf(pr -> pr.getSubjectId().equals(id));
+    }
 
 }
